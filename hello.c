7eeaa0633a368b29ae6d490f71c79dee8c2af331
unknown <shahbazshekh>
2024-07-27 14:14:08 -0700

    first hello world C file

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdipo.h>
+int main(){
+    printf("Hello world\n");
+    printf("My name is shahbaz\n");
+    printf("My name is shahbaz\n");
+    printf("My name is shahbaz\n");
+    printf("My name is shahbaz\n");
+    printf("My name is c1\n");
+    
+    return 0;
+}
